5a9fb9ccab44c29461b5b863410cbfd63a5a3503
Abbas Jafari <abbas.jafari.4001@gmail.com>
2023-10-29 21:39:24 +0330

    update

--- app/[lang]/api/category/route.ts
+++ app/[lang]/api/category/route.ts
@@ -4,7 +4,7 @@ import { Params } from "_types";
 export async function GET(request: Request, { params }: Params) {
   const url = new URL(request.url);
 
-  const query = `*[_type=="category_${params.lang}"]|order(number asc)`;
+  const query = `*[_type=="category_${params.lang}"]`;
   const data = await client.fetch(query);
 
   return Response.json(data);
