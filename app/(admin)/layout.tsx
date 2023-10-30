18f4e1c0af4be8487353552e08fd1e6b06ef6875
Abbas Jafari <abbas.jafari.4001@gmail.com>
2023-10-30 14:24:55 +0330

    update

--- app/(admin)/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { Children } from "_types";
-import React from "react";
-
-export const dynamicParams = true;
-
-export default function AdminLayuot({ children }: Children) {
-  return <>{children}</>;
-}
