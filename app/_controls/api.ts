745fb0bc5aca5bb7627bace530c566d28c949673
Abbas Jafari <abbas.jafari.4001@gmail.com>
2023-10-30 08:01:21 +0330

    update

--- app/_controls/api.ts
+++ app/_controls/api.ts
@@ -2,7 +2,7 @@ const headers = {
   "Content-Type": "application/json",
   Accept: "application/json",
 };
-const site_url = "https://abbasjafari-vu.github.io" ?? "http://localhost:3000";
+const site_url = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
 
 const api = (url: string, lang: string, option?: object) =>
   fetch(`${site_url}/${lang}/api${url}`, { headers, ...option }).then((res) =>
