3d03b8e033c41e7cb75304ff60bb16d3c70de004
Abbas Jafari <abbas.jafari.4001@gmail.com>
2023-10-30 14:18:02 +0330

    update

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -2,7 +2,7 @@
 import { Children } from "_types";
 import React from "react";
 
-export const dynamicParams = false;
+export const dynamicParams = true;
 
 export default function AdminLayuot({ children }: Children) {
   return <>{children}</>;
