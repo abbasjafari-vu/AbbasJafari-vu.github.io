646f886efbfe3df7a6424ea2cf72c9f14257ce10
Abbas Jafari <abbas.jafari.4001@gmail.com>
2023-10-30 11:53:55 +0330

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "_styles/globals.css";
 import { Children, Params } from "_types";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,7 @@ function getLocale(request: NextRequest): string | undefined {
   const negotiatorHeaders: Record<string, string> = {};
 
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-  // negotiatorHeaders["accept-language"] = i18n.defaultLocale;
+  negotiatorHeaders["accept-language"] = i18n.defaultLocale;
 
   // @ts-ignore locales are readonly
   const locales: string[] = i18n.locales;
